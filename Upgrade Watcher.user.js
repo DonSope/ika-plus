e495539d57b10724cab4022be9888e4a77e92dba
DonSope <DonSope@users.noreply.github.com>
2016-02-12 00:41:38 +0100

    Update Upgrade Watcher.user.js

--- Upgrade Watcher.user.js
+++ Upgrade Watcher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Ikariam Upgrade Watcher 3.0
-// @version        2.6.2
+// @version        2.6.3
 // @namespace      ikariam
 // @description    Attaches a simple icon on each building which show level and if upgradeable
 // @author         Apollo ヅ, mrkokaney, DonSope
